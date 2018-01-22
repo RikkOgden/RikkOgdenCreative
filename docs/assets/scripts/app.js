f976c18812246c8a003cc432c0b106206c923f9b
RikkOgden <rikkogden@gmail.com>
2018-01-22 12:41:55 +0000

    Cleaned up build task for only minified assets

--- docs/assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
-new MobileMenu('.site-header__menu-icon', '.nav-primary');
-new RevealOnScroll('.feature-item', '100%');
-new StickyHeader();
-new Modal();
